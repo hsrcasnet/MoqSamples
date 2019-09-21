269307ac29927a9a421d5acabc7954bf6893a45d
Thomas Galliker <thomas@bluewin.ch>
2019-09-21 11:18:26 +0200

    Add tests to demonstrate Verify + VerifyAll

--- MoqSamples.Tests/PropertyMockingTests.cs
+++ MoqSamples.Tests/PropertyMockingTests.cs
@@ -37,5 +37,44 @@ namespace MoqSamples.Tests
             personRepositoryMock.Verify(r => r.DeletePerson(It.Is<Person>(p => p.Name == "Marcel")), Times.Exactly(1));
             personRepositoryMock.Verify(r => r.DeletePerson(It.Is<Person>(p => p.Name == "Joseph")), Times.Exactly(1));
         }
+
+        [Fact]
+        public void ShouldDeleteRetiredPersonsByAge_UsingVerifications()
+        {
+            // Arrange
+            var testPersons = new List<Person>
+            {
+                new Person { Name = "Martin", Birthdate = new DateTime(2010, 12, 31) },
+                new Person { Name = "Thomas", Birthdate = new DateTime(1986, 07, 11) },
+                new Person { Name = "Marcel", Birthdate = new DateTime(1940, 01, 01) },
+                new Person { Name = "Joseph", Birthdate = new DateTime(1920, 01, 01) },
+            };
+
+            var dateTimeMock = new Mock<IDateTime>();
+            dateTimeMock.SetupGet(d => d.Now).Returns(new DateTime(2019, 01, 01));
+
+            var personRepositoryMock = new Mock<IPersonRepository>();
+            personRepositoryMock.Setup(r => r.GetPersons()).Returns(testPersons).Verifiable();
+            personRepositoryMock.Setup(r => r.DeletePerson(It.Is<Person>(p => p.Name == "Marcel"))).Verifiable();
+            personRepositoryMock.Setup(r => r.DeletePerson(It.Is<Person>(p => p.Name == "Joseph"))).Verifiable();
+
+            // DEMO1: Setup an unmatched method call 
+            personRepositoryMock.Setup(r => r.DeletePerson(It.Is<Person>(p => p.Name == "Test")));
+
+            var propertyMocking = new PropertyMocking(dateTimeMock.Object, personRepositoryMock.Object);
+
+            // Act
+            propertyMocking.DeleteRetiredPersons(age: 65);
+
+            // Assert
+            // DEMO1: Verify() checks all Setups marked with .Verifiable()
+            personRepositoryMock.VerifyAll();
+
+            // DEMO2: VerifyAll() checks all Setups - regardless if they're marked with .Verifiable()
+            personRepositoryMock.Verify();
+
+            // DEMO3: VerifyNoOtherCalls fails if unverified invocations happened
+            personRepositoryMock.VerifyNoOtherCalls();
+        }
     }
 }
